4dc86ae313e09a283cd749ddf5f420461939d8e6
Dimitri Kontos <ckontos@rocketmail.com>
2017-12-12 04:10:59 +0000

    cleaned up

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ var path = require('path');
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-//app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(bodyParser.json());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
